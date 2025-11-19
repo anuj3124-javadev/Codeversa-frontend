f7a259b032c790f6ec528768bab880b4ce952db1
anuj3124-javadev <anujyadavst24@gmail.com>
2025-11-19 23:27:58 +0530

    Add link

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://codeversa-backend.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
