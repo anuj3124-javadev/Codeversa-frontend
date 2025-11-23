101bf4ab99bb5017361cac7756ae3ca02f3cb744
anuj3124-javadev <anujyadavst24@gmail.com>
2025-11-23 11:54:22 +0530

    your commit message

--- src/config/constants.js
+++ src/config/constants.js
@@ -66,11 +66,13 @@ int main() {
     // Calculate factorial
     int number = 5;
     long factorial = 1;
+
     for(int i = 1; i <= number; i++) {
         factorial *= i;
     }
-    cout << "Factorial of " + number + " is: " + factorial << endl;
-    
+
+    cout << "Factorial of " << number << " is: " << factorial << endl;
+
     return 0;
 }`,
 
